a23149bdcfa9b69b2390088599b2fb80ee92688e
SuyashLakhotia <suyashlakhotia@gmail.com>
2015-02-08 00:17:44 +0800

    Changed app theme.

--- app/src/main/java/com/example/suyashl/whybills/ReceiptsView.java
+++ app/src/main/java/com/example/suyashl/whybills/ReceiptsView.java
@@ -1,33 +1,21 @@
 package com.example.suyashl.whybills;
 
+import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.app.ListActivity;
 
 
-public class ReceiptsView extends ActionBarActivity {
+public class ReceiptsView extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipts_view);
-    }
-
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }

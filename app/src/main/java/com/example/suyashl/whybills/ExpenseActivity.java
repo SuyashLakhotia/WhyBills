fddb16ffcefcffbc1469ada9f24d423289c57699
SuyashLakhotia <suyashlakhotia@gmail.com>
2015-02-08 00:03:22 +0800

    Removed Options Menu.

--- app/src/main/java/com/example/suyashl/whybills/ExpenseActivity.java
+++ app/src/main/java/com/example/suyashl/whybills/ExpenseActivity.java
@@ -28,13 +28,6 @@ public class ExpenseActivity extends ListActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_expense, menu);
-        return true;
-    }
-
     private void fillData() {
         Cursor remindersCursor = dbHelper.fetchAllExpenses();
         startManagingCursor(remindersCursor);
--- app/src/main/java/com/example/suyashl/whybills/ReceiptsView.java
+++ app/src/main/java/com/example/suyashl/whybills/ReceiptsView.java
@@ -15,12 +15,6 @@ public class ReceiptsView extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_receipts_view, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
